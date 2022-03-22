7dd8f8a044b8388fb59b6968587ece1180391418
amiria703 <amiria703@gmail.com>
2022-03-23 03:58:26 +0430

    transfer workflow to docs branch and add package-lock.json

---
+++ docs/.vitepress/dist/assets/examples_examples.md.6f570dae.js
@@ -0,0 +1,28 @@
+import{_ as s,c as n,o as a,a as t}from"./app.b681bcea.js";const h='{"title":"Examples","description":"","frontmatter":{},"headers":[{"level":2,"title":"cls.js","slug":"cls-js"},{"level":2,"title":"Hello World!","slug":"hello-world"},{"level":2,"title":"Hi CLS!","slug":"hi-cls"}],"relativePath":"examples/examples.md","lastUpdated":1647988902000}',p={},e=t(`<h1 id="examples" tabindex="-1">Examples <a class="header-anchor" href="#examples" aria-hidden="true">#</a></h1><h2 id="cls-js" tabindex="-1">cls.js <a class="header-anchor" href="#cls-js" aria-hidden="true">#</a></h2><div class="language-javascript"><pre><code><span class="token function">style</span><span class="token punctuation">(</span><span class="token punctuation">{</span>
+	<span class="token literal-property property">text</span><span class="token operator">:</span> <span class="token string">&quot;CLS.JS is great!&quot;</span><span class="token punctuation">,</span>
+    <span class="token literal-property property">color</span><span class="token operator">:</span> <span class="token string">&quot;purple&quot;</span><span class="token punctuation">,</span>
+    <span class="token literal-property property">animation</span><span class="token operator">:</span> <span class="token punctuation">{</span>
+        <span class="token literal-property property">right</span><span class="token operator">:</span> <span class="token number">20</span><span class="token punctuation">,</span>
+        <span class="token literal-property property">timer</span><span class="token operator">:</span> <span class="token number">100</span><span class="token punctuation">,</span>
+    <span class="token punctuation">}</span>
+<span class="token punctuation">}</span><span class="token punctuation">)</span><span class="token punctuation">;</span>
+</code></pre></div><h2 id="hello-world" tabindex="-1">Hello World! <a class="header-anchor" href="#hello-world" aria-hidden="true">#</a></h2><div class="language-javascript"><pre><code><span class="token function">style</span><span class="token punctuation">(</span><span class="token punctuation">{</span>
+	<span class="token literal-property property">text</span><span class="token operator">:</span> <span class="token string">&quot;Hello World!&quot;</span><span class="token punctuation">,</span>
+    <span class="token literal-property property">color</span><span class="token operator">:</span> <span class="token string">&quot;rgb:0;0;0&quot;</span><span class="token punctuation">,</span>
+    <span class="token literal-property property">bgColor</span><span class="token operator">:</span> <span class="token string">&quot;rgb:255;225;225&quot;</span><span class="token punctuation">,</span>
+    <span class="token literal-property property">animation</span><span class="token operator">:</span> <span class="token punctuation">{</span>
+        <span class="token literal-property property">right</span><span class="token operator">:</span> <span class="token number">50</span><span class="token punctuation">,</span>
+        <span class="token literal-property property">timer</span><span class="token operator">:</span> <span class="token number">50</span><span class="token punctuation">,</span>
+    <span class="token punctuation">}</span><span class="token punctuation">,</span>
+    <span class="token literal-property property">font_style</span><span class="token operator">:</span> <span class="token string">&quot;bold italic&quot;</span>
+<span class="token punctuation">}</span><span class="token punctuation">)</span><span class="token punctuation">;</span>
+</code></pre></div><h2 id="hi-cls" tabindex="-1">Hi CLS! <a class="header-anchor" href="#hi-cls" aria-hidden="true">#</a></h2><div class="language-javascript"><pre><code><span class="token function">style</span><span class="token punctuation">(</span><span class="token punctuation">{</span>
+	<span class="token literal-property property">text</span><span class="token operator">:</span> <span class="token string">&quot;Hi CLS!&quot;</span><span class="token punctuation">,</span>
+    <span class="token literal-property property">color</span><span class="token operator">:</span> <span class="token string">&quot;whitesmoke&quot;</span><span class="token punctuation">,</span>
+    <span class="token literal-property property">animation</span><span class="token operator">:</span> <span class="token punctuation">{</span>
+        <span class="token literal-property property">right</span><span class="token operator">:</span> <span class="token number">50</span><span class="token punctuation">,</span>
+        <span class="token literal-property property">timer</span><span class="token operator">:</span> <span class="token number">5</span><span class="token punctuation">,</span>
+    <span class="token punctuation">}</span><span class="token punctuation">,</span>
+    <span class="token literal-property property">font_style</span><span class="token operator">:</span> <span class="token string">&quot;reverse&quot;</span>
+<span class="token punctuation">}</span><span class="token punctuation">)</span><span class="token punctuation">;</span>
+</code></pre></div>`,7),o=[e];function r(l,c,i,u,k,d){return a(),n("div",null,o)}var _=s(p,[["render",r]]);export{h as __pageData,_ as default};
